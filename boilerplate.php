5f43737010e2eae9ec01a8d5bc574d6539315d8d
Fahmii19 <fhmialfi79@gmail.com>
2022-10-19 07:34:23 +0700

    push pratikum

--- boilerplate.php
+++ boilerplate.php
@@ -4,17 +4,22 @@
 class Animal
 {
     # property animals
+    public $animals;
 
     # method constructor - mengisi data awal
     # parameter: data hewan (array)
     public function __construct($data)
     {
+        $this->animals = $data;
     }
 
     # method index - menampilkan data animals
     public function index()
     {
         # gunakan foreach untuk menampilkan data animals (array)
+        foreach ($this->animals as $animal) {
+            echo $animal . '<br>';
+        }
     }
 
     # method store - menambahkan hewan baru
@@ -22,12 +27,14 @@ class Animal
     public function store($data)
     {
         # gunakan method array_push untuk menambahkan data baru
+        array_push($this->animals, $data);
     }
 
     # method update - mengupdate hewan
     # parameter: index dan hewan baru
     public function update($index, $data)
     {
+        $this->animals[$index] = $data;
     }
 
     # method delete - menghapus hewan
@@ -35,12 +42,13 @@ class Animal
     public function destroy($index)
     {
         # gunakan method unset atau array_splice untuk menghapus data array
+        unset($this->animals[$index]);
     }
 }
 
 # membuat object
 # kirimkan data hewan (array) ke constructor
-$animal = new Animal([]);
+$animal = new Animal(['Crocodile', 'Tiger', 'Elephant']);
 
 echo "Index - Menampilkan seluruh hewan <br>";
 $animal->index();
